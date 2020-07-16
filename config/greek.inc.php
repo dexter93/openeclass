8d9cc48b11794db40fff476292474a1690af89e4
Dimitris Mantzouranis <d3xter93@gmail.com>
2020-07-16 17:51:02 +0300

    lang: customise some strings

---
+++ config/greek.inc.php
@@ -0,0 +1,24 @@
+<?php
+$langEclass = "Semfe.gr";
+
+$langTeacher = "Καθηγητής";
+$langOfTeacher = "Καθηγητή";
+$langTeachers = "Καθηγητές";
+$langOfTeachers = "Καθηγητών";
+
+$langsTeacher = "καθηγητής";
+$langsOfTeacher = "καθηγητή";
+$langsTeachers = "καθηγητές";
+$langsOfTeachers = "καθηγητών";
+
+$langStudent = "Φοιτητής";
+$langOfStudent = "Φοιτητή";
+$langStudents = "Φοιτητές";
+$langOfStudents = "Φοιτητών";
+
+$langsStudent = "φοιτητής";
+$langsOfStudent = "φοιτητή";
+$langsStudents = "φοιτητές";
+$langsOfStudents = "φοιτητών";
+$langsOfStudentss = "φοιτητές";
+$langsstudent_acc = "φοιτητή";
