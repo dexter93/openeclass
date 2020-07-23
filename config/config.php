1a40c7628776ffe2e161c5a8139090d7b62cd937
Dimitris Mantzouranis <d3xter93@gmail.com>
2020-07-23 16:32:23 +0300

    composer update
    AGAIN

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+/* ========================================================
+ * Open eClass 3.x configuration file
+ * Created by install on 2020-07-23 16:28
+ * ======================================================== */
+
+$mysqlServer = 'localhost';
+$mysqlUser = 'homestead';
+$mysqlPassword = 'secret';
+$mysqlMainDb = 'eclass';
